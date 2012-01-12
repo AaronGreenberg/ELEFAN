d44d45fd6ac6f735a608dd89d106f7c81e57795d
Aaron Greenberg <daft@debian.(none)>
2012-01-12 10:25:22 -0800

    local work ESP

--- main.R
+++ main.R
@@ -1,8 +1,11 @@
 require(PBSmodelling);
 createWin("ELEFAN.txt");
 source("ASP.R")
+source("ESP.R")
 source("LF_plots.R")
 data<-read.table("test.dat",head=TRUE)  #read in data
+
+
 
 curves <- function(ti=1:12){#plots
   getWinVal(scope="L");
@@ -10,12 +13,31 @@ curves <- function(ti=1:12){#plots
 return(c)
 }
 
-data <-main(data) 
 
+curves2 <- function(ti=1:12,Linf,tw,K){#plots #for optimization
+  
+  c <- Linf*(1-exp(-K*(ti-tw)-sin(2*pi*(ti-tw))))
+return(c)
+}
+
+
+data <-main(data)
+data2 <- main2(data)
 timecurves <- 1:12
-#c1 <- curves(1:12,35,.9,.2)
+
 plotdata <- as.data.frame(cbind(data$F,data$F,data$F,data$F,data$F,data$F,data$F,data$F,data$F,data$F,data$F,data$F))
 plotdata2 <- as.data.frame(cbind(data$OBS,data$OBS,data$OBS,data$OBS,data$OBS,data$OBS,data$OBS,data$OBS,data$OBS,data$OBS,data$OBS,data$OBS))
+
+
+#ESP
+ESP <- function(Linf,tw,K,pdata){
+  cur <- curves2(1:12,Linf,tw,K)
+  for(i in 1:length(cur)){
+    if
+  
+  }
+}
+
 
 plotlf <- function(da=data,pd=plotdata2){
   c1 <- curves(1:12) 
