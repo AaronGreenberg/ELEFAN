2e23235fd77ec6e1a37dd0cb8fc0023ba8b1e4b5
Aaron Greenberg <daft@debian.(none)>
2012-10-01 14:21:01 -0700

    adding gui goodness

--- main.R
+++ main.R
@@ -11,25 +11,75 @@ source("ESP.R")                         #load routines to compute Explained Sum 
 source("LF_plots.R")                    #load routines to make the special plots in particular rqFreqplot
 source("other_plots.R")                 #load routines that make other plots
 source("low_level.R")                   #load misc routines really mostly data proccessing     
+createWin("main_gui.txt")
 
-createWin("ELEFAN.txt");                #Make gui
-buildgui <- function()
-  {                                     #This just sets initial bounds on the GUI
-    setWinVal(list(Linf.min=0,Linf.max=2*max(data$ML),Linf=max(data$ML)))
-    setWinVal(list(points.min=3,points.max=length(data$ML),points=3))
-  }
+
 
 ## % Read in the data 
 ## %############################################################
 ## %############################################################
 ## %
 
-data<-read.table("test.dat",head=TRUE,as.is=TRUE)      #read in the length frequency data
-date<-read.table("date_test.dat",head=TRUE,as.is=TRUE)  #read in date key for the length frequency data This should probably be documented
+datefilein <- function(){
+fname1 <- selectFile()
+date<-read.table(fname1,head=TRUE,as.is=TRUE)  #read in date key for the length frequency data This should probably be documented
+assign("date", date, envir = .GlobalEnv)
+}
+
+
+lffilein <- function(){
+fname2 <- selectFile()
+data<-read.table(fname2,head=TRUE,as.is=TRUE)  #read in date key for the length frequency data This should probably be documented
+assign("data", data, envir = .GlobalEnv)
+}
+
+
+
+
+## buildgui <- function()
+##   {                                     #This just sets initial bounds on the GUI
+##     setWinVal(list(Linf.min=0,Linf.max=2*max(data$ML),Linf=max(data$ML)))
+##     
+##   }
+
 date$Date=(as.Date(date$Date,format="%d/%m/%y"))       #convert dates into the date class.
 datelength <- length(date$Date)                                      #get number of days data was collected
 days= as.numeric(julian(date$Date[datelength])-julian(date$Date[1])) #set default number of days
 moddays=(365-days%%365)+days                                         #compute width of plot window in years... 
 days=moddays                                                         #reset days
 lfbin=length(data$ML)                   #get number of bins
-buildgui()
+
+
+
+lfgui <- function()
+{
+  createWin("lf_gui.txt")
+  setWinVal(list(Linf.min=0,Linf.max=2*max(data$ML),Linf=max(data$ML)))
+
+}
+
+
+
+Kscangui <- function()
+{
+  createWin("kscan_gui.txt")
+  
+  print("This is still dummy")
+}
+
+Weatherallgui<- function()
+{
+  createWin("wetherall_gui.txt")
+  setWinVal(list(points.min=2,points.max=length(data$ML),points=3))
+}
+
+
+Catchcurvegui<- function()
+{
+  createWin("catch_gui.txt")
+  
+}
+
+return2main <- function(){createWin("main_gui.txt")}
+
+## #buildgui()
