6ee39e591e47d149c8b4a3173dba71e4509011e5
Danielle Knip <d.knip@fisheries.ubc.ca>
2013-05-29 12:52:13 -0700

    new file and plot yield per recruit edits

--- R/plot_yield_biomass_per_recruit.r
+++ R/plot_yield_biomass_per_recruit.r
@@ -4,8 +4,11 @@
 
 plot_yield_biomass_per_recruit <- function(tab_final)
 {
-par(mfrow=c(2,1), mar=c(4,4,0.5,0.5), oma=c(0,1,5,0))
+par(mfrow=c(2,1), mar=c(6,6,0.5,0.5), mgp=c(4,1,0), oma=c(0,1,1,1))
 is.na(tab_final) <- is.na(tab_final)
+
+
+
 
 ylim <- c(0,max(tab_final[,1:2],na.rm=TRUE))
 plot(rownames(tab_final),tab_final[,1],type="l",xaxt="n",yaxt="n",ylab="Relative yield per recruit",xlab="",col=1,ylim=ylim)
@@ -18,6 +21,5 @@ plot(rownames(tab_final),tab_final[,3],type="l",xaxt="n",yaxt="n",ylab="Relative
 lines(rownames(tab_final),tab_final[,4],type="l",lty=2,col=1)
 axis(1,tck=0.02)
 axis(2,tck=0.02,las=2)
-
 
 }
