96b1932d53f390d1d6818a2b0bfe987fbfb358f6
Aaron Greenberg <daft@crabby.www.moondoggie2.org>
2013-08-16 12:30:39 -0700

    committing catch curve two small modifications and better comments of Catch curve one

--- R/catchcurves.R
+++ R/catchcurves.R
@@ -25,18 +25,18 @@ plotnonseacatchcurve <- function(Kloc=K,Linfloc=Linf,pointsupper,pointslower){
   ti <- -1/Kloc*log(1-(dataloc$ML)/Linfloc)
 sumsample <- ifelse(sumsample==0,mean(sumsample),sumsample)#get rid of zeros
 #Ok time to make the plot
-widthvec <- pointslower:(pointsupper)
+widthvec <- pointslower:(pointsupper) #get vector of circles used in linear regression.
 print(ti)
-widthvec2 <-0:pointslower
-z <- lm(log(sumsample[widthvec]/delti[widthvec])~ti[widthvec])
+widthvec2 <-0:pointslower#points not selected
+z <- lm(log(sumsample[widthvec]/delti[widthvec])~ti[widthvec])#compute linear model.
 par(1,las=1,bty='n',oma=c(0,1,1,1))
-plot(x=ti[widthvec2],y=(z$coefficients[1]+z$coefficients[2]*ti[widthvec2]),type="l",lty=2,col="black",xlab=bquote(paste("Relative age (t-t"[o],")")), ylab=expression(paste("Relative abundance (ln(N/",Delta,"t))")),yaxt="n",xaxt="n",xlim=c(-0.5,ceiling(max(ti))),ylim=c(0,ceiling(1.1*z$coefficients[1])))#make the line that does not count
+plot(x=ti[widthvec2],y=(z$coefficients[1]+z$coefficients[2]*ti[widthvec2]),type="l",lty=2,col="black",xlab=bquote(paste("Relative age (t-t"[o],")")), ylab=expression(paste("Relative abundance (ln(N/",Delta,"t))")),yaxt="n",xaxt="n",xlim=c(-0.5,ceiling(max(ti))),ylim=c(0,ceiling(1.1*z$coefficients[1])))#make the line that does not count goes trough widthvec2 ... the points NOT selected
 axis(2,tck=0.02,las=2)
 axis(1,tck=0.02)
 
-points(ti,log(sumsample/delti))
-text(ti,log(sumsample/delti)+.2*log(max(log(sumsample/delti))),as.character(1:length(ti)))
-points(ti[widthvec],log(sumsample[widthvec]/delti[widthvec]),pch=19,col="black")
+points(ti,log(sumsample/delti))#make open circles
+text(ti,log(sumsample/delti)+.2*log(max(log(sumsample/delti))),as.character(1:length(ti))) #put on text 
+points(ti[widthvec],log(sumsample[widthvec]/delti[widthvec]),pch=19,col="black") #make filled circles only on points in width vect
 
 lines(x=ti[widthvec],y=(z$coefficients[1]+z$coefficients[2]*ti[widthvec]),col="black")#make the line through the selected points
 
@@ -68,14 +68,7 @@ plotseacatchcurve<- function(Kloc=K,Linfloc=Linf,Cloc=C,TW=Tw,pointsupper,points
   #--1--compute oldest and youngest
   #locate the oldest and youngest fish.
   index <-which(colSums(lfdata)>0)
-  ## print("index where lfdata not equal zero")
-  ## print(index)
-  ## print(colSums(lfdata)[index])
-  ## print(max(index))
-  ## print(min(index))
-  ## print(lfdata[,max(index)])
-  ## print(lfdata[,min(index)])
-  youngest <- max(index)#lfdata[1,max(index)]
+   youngest <- max(index)#lfdata[1,max(index)]
   oldest <- min(index)#lfdata[length(datain$ML),min(index)]
   #youngest <- max(which(lfdata[1,]>0))            
   #oldest <- max(which(lfdata[length(lfdata[,1]),]>0)) 
@@ -142,7 +135,7 @@ plotseacatchcurve<- function(Kloc=K,Linfloc=Linf,Cloc=C,TW=Tw,pointsupper,points
   }
   }
  }
-  #pointsout <- prop.table(pointsout,2)
+  pointsout <- prop.table(pointsout,2)*100
   ages <- vector()
   for(i in 1:length(tzero)){#loop over curves
   tempered <- curves_cpp(Linfloc,Cloc,TW,Kloc,datain$ML,days,tzero[i],0,BIRTHDAY)
@@ -160,7 +153,7 @@ print(y)
 z <- lm(y~x)
 print(summary(z))
 
-
+#should follow what I did in Catch curve 1.
 par(1,las=1,bty='n',oma=c(0,1,1,1))
 plot(ages/365,log(rowSums(pointsout)),xlab=bquote(paste("Relative age (t-t"[o],")")),ylab=expression(paste("Relative abundance (ln(N))")),yaxt="n",xaxt="n")
 axis(2,tck=0.02,las=2)
