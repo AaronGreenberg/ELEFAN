dd7716df2eed45d76225713172cc96456af40319
Aaron Greenberg <daft@debian>
2014-01-24 15:01:22 -0800

    committing catch curve

--- R/catchcurves.R
+++ R/catchcurves.R
@@ -65,19 +65,18 @@ plotseacatchcurve<- function(Kloc=K,Linfloc=Linf,Cloc=C,TW=Tw,pointsupper,points
   #--1--compute oldest and youngest
   #locate the oldest and youngest fish.
   index <-which(colSums(lfdata)>0)
-   youngest <- max(index)#lfdata[1,max(index)]
-  oldest <- min(index)#lfdata[length(datain$ML),min(index)]
-  #youngest <- max(which(lfdata[1,]>0))            
-  #oldest <- max(which(lfdata[length(lfdata[,1]),]>0)) 
-  #print("hum")
-  # compute growth curve that goes through oldest and youngest
-
+  youngest <- max(index)  #lfdata[1,max(index)]
+  oldest <- min(index)    #lfdata[length(datain$ML),min(index)]
+  
+ #2 compute growth curve that goes through oldest and youngest
   gcurve1 <- curves_cpp(Linfloc,Cloc,TW,Kloc,datain$ML,days,youngest,datain$ML[1],BIRTHDAY)#compute growth curve that goes through oldest
   gcurve2 <- curves_cpp(Linfloc,Cloc,TW,Kloc,datain$ML,days,oldest,datain$ML[length(datain$ML)],BIRTHDAY)#compute growth curve that goes through youngest
   #index <-which(colSums(lfdata)>0)
   
   #find vector of tzeros that determine slicing growth curves
-  tzero <- floor(sort(seq(oldest+gcurve2$tzero,youngest+gcurve1$tzero,length.out=length(datain$ML)),decreasing=TRUE))#(length(datain$ML)+1)),decreasing=TRUE))
+  #tzero <- floor(sort(seq(oldest+gcurve2$tzero,youngest+gcurve1$tzero,length.out=length(datain$ML)),decreasing=TRUE))#(length(datain$ML)+1)),decreasing=TRUE))
+  tzero <- floor(sort(seq(oldest+gcurve2$tzero,youngest+gcurve1$tzero,length.out=3),decreasing=TRUE))#(length(datain$ML)+1)),decreasing=TRUE))
+  #3 compute pointscurve this computes the growth curves.
 # gcurvemain <- 0*lfdata
  count=1
   pointscurve <- matrix(0,ncol=4,nrow=length(tzero)*length(index))
@@ -104,69 +103,116 @@ plotseacatchcurve<- function(Kloc=K,Linfloc=Linf,Cloc=C,TW=Tw,pointsupper,points
     }
  }
 #The above makes the table. of growth curves
+  #now we need to intersect the growth curves and the data
+  #and add things up. This is the part with problems
+
   
   pointcurve <- as.data.frame(pointscurve)
   colnames(pointcurve) <- c("curve","day","length","bin")
   print(pointcurve)
   pointsout <- matrix(0,nrow=length(tzero),ncol=length(index))
   #loop over days
+  
   for(i in 1:length(index)){
   #loop over correct number of curves and fill in table.
    z=as.data.frame(subset(pointcurve,pointcurve$day==index[i])) #get part of point curve table that corresponds to correct day
    colnames(z) <- c("curve","day","length","bin") #get part that is correct day.
    subday=z#correct subday!
-   #get bins between curves
    count=0;
-   for(j in 1:(length(tzero)-1)){ #for each growth curve 
-     curvebin <- which(datain$ML >= floor(subday$length[j]) &datain$ML <= ceiling(subday$length[j+1]))#get bins between growth curves
-     if(!is.na(sum(datain[curvebin,i+1]))){#check that curvebin is not empty
-       lengthcurvebin <- length(curvebin)#get length of curve bin
-       binwidth <- (datain$ML[2]-datain$ML[1]) #compute width of bins
-       if(lengthcurvebin>0){#if length of curve bin is bigger than zero compute weights
-       #make sure that the length of curve bin is greater than zero
+   for(j in 1:(length(tzero)-1)){ #for each growth curve
+     #this works!
+     weight1=1
+     weight2=1
+     binwidth <- (datain$ML[2]-datain$ML[1]) #compute width of bins
+     upper <- ceiling(subday$length[j+1])
+     lower<- floor(subday$length[j])
+     if(is.na(subday$length[j+1])){upper=max(datain$ML)+binwidth/2}
+     if(is.na(subday$length[j])){lower=min(datain$ML)-binwidth/2}
+     print("subday")
+     print(subday$length[j])
+     print("lower")
+     print(lower)
+     print("ceiling")
+     print(ceiling(subday$length[j+1]))
+     print("upper")
+     print(upper)
+
+     curvebin <- which(datain$ML >= lower & datain$ML <= upper)#get bins between growth curves
+     print(curvebin)
+     print(c(i,j))
+      #curvebin <- which(datain$ML >= subday$bin[j] & datain$ML <=subday$bin[j+1])#get bins between growth curves
+      if(!is.na(sum(datain[curvebin,i+1]))){#check that curvebin is not empty
+        lengthcurvebin <- length(curvebin)#get length of curve bin
+
+        #seems to work up to now!
+        if(lengthcurvebin>0){#if length of curve bin is bigger than zero compute weights
+        #make sure that the length of curve bin is greater than zero
        if(!is.na(subday$length[curvebin[1]])&&(subday$length[curvebin[1]]>=(min(datain$ML)-binwidth/2))){
-       #we need to compute two weights which determine the correct weights for the
-         
-       weight1=(subday$bin[curvebin[1]]-subday$length[curvebin[1]])/(2*binwidth)+.5}else{weight1=1}
-       if(!is.na(subday$length[curvebin[lengthcurvebin]])&&(subday$length[curvebin[lengthcurvebin]]<=(max(datain$ML)+binwidth/2))){
-       weight2=(subday$length[curvebin[lengthcurvebin]]-subday$bin[curvebin[lengthcurvebin]])/(2*binwidth) +.5
-      }else{weight2=1}  
+        #we need to compute two weights which determine the correct weights for the Are these right?!
+         if(subday$length[curvebin[1]]<subday$bin[curvebin[1]]){
+       weight1=.5-(subday$bin[curvebin[1]]-subday$length[curvebin[1]])/(binwidth)
      }else{
-       weight1=0
-       weight2=0
+       weight1=.5+(-subday$bin[curvebin[1]]+subday$length[curvebin[1]])/(binwidth)
      }
+      }
        
-       if(max(c(weight1,weight2))>1||min(c(weight1,weight2)<0)){#error check. Weights should be between zero and one
-       print("Weights")
+       else{weight1=1}
+       if(!is.na(subday$length[curvebin[lengthcurvebin]])&&(subday$length[curvebin[lengthcurvebin]]<=(max(datain$ML)+binwidth/2))){
+         if(subday$length[curvebin[lengthcurvebin]]<subday$bin[curvebin[lengthcurvebin]]){
+       weight2=.5-(subday$length[curvebin[lengthcurvebin]]-subday$bin[curvebin[lengthcurvebin]])/(binwidth) 
+     }else{
+       weight2=(-subday$length[curvebin[lengthcurvebin]]+subday$bin[curvebin[lengthcurvebin]])/(binwidth) +.5}
+      }else{weight2=1}  
+
+     }
+       if(max(c(weight1,weight2))>1){#error check. Weights should be between zero and one
+       print("Weights Error weights greater than one")
        print(c(weight1,weight2))
        print(j)
        print(subday$length[curvebin])
        print(subday$bin[curvebin])
-       
-       #if(weight1>1){weight1=1}
-       #if(weight2>1){weight2=1}
       
-     } 
-       if(lengthcurvebin>=3){#if there are more than three bins in curvebin
+     }
+       if(min(c(weight1,weight2))<0){#error check. Weights should be between zero and one
+       print("Weights Error less than one")
+       print(c(weight1,weight2))
+       print(j)
+       print(subday$length[curvebin])
+       print(subday$bin[curvebin])
+      
+     }  
+        ## print("curvebin loop")
+        ## print(curvebin)
+        ## print(datain[curvebin,i+1])
+        ## print(sum(datain[curvebin,i+1]))
+        ## print(c(i,j))
+        if(lengthcurvebin>=3){#if there are more than three bins in curvebin
          pointsout[subday$curve[j],i] <- sum(datain[curvebin[2:(lengthcurvebin-1)],i+1])#add up all things in middle
-         pointsout[subday$curve[j],i] <- pointsout[subday$curve[j],i]+datain[curvebin[1],i+1]*weight1 # add lower bin
+         pointsout[subday$curve[j],i] <- pointsout[subday$curve[j],i]+datain[curvebin[1],i+1]*(1-weight1) # add lower bin
          pointsout[subday$curve[j],i] <- pointsout[subday$curve[j],i]+datain[curvebin[lengthcurvebin],i+1]*weight2# add upper bin
+         print("pointsout")
+         print(pointsout)
        }else{
          if(lengthcurvebin==2){#if there are just two bins in the curvebin
-           pointsout[subday$curve[j],i] <- datain[curvebin[1],i+1]*weight1 # add lower bin
+           pointsout[subday$curve[j],i] <- datain[curvebin[1],i+1]*(1-weight1) # add lower bin
            pointsout[subday$curve[j],i] <- pointsout[subday$curve[j],i]+datain[curvebin[2],i+1]*weight2# add upper bin
+         print("pointsout")
+         print(pointsout)
          }else{
            if(lengthcurvebin==1){#if there is just one bin? Am I doing this right?
-           pointsout[subday$curve[j],i] <- datain[curvebin[1],i+1]*weight1 # add lower bin
+           pointsout[subday$curve[j],i] <- datain[curvebin[1],i+1]*(1-weight1) # add lower bin
+         print("pointsout")
+         print(pointsout)
            }else{ #this should never happen ? right?
-           pointsout[subday$curve[j],i] <- 0
+             print("OH KNOW!!!")
+           pointsout[subday$curve[j],i] <- .1
          }}
        }
      }
    }
  }
 
-  #pointsout <- prop.table(pointsout,2)*100
+#  pointsout <- prop.table(pointsout,2)*100#normalize the columns
   ages <- vector()#compute ages for plotting
   for(i in 1:length(tzero)){#loop over curves
   tempered <- curves_cpp(Linfloc,Cloc,TW,Kloc,datain$ML,days,tzero[i],0,BIRTHDAY)
@@ -175,29 +221,23 @@ plotseacatchcurve<- function(Kloc=K,Linfloc=Linf,Cloc=C,TW=Tw,pointsupper,points
 
   pointsout <- pointsout[nrow(pointsout):1,]# got to reverse order so youngest fish are plotted first!
   widthvec <- (pointslower):pointsupper
-
   widthvec2 <-c(1:pointslower, pointsupper:nrow(pointsout))
-#  print("width vec")
-#  print(widthvec)
-#  print(widthvec2)
   
 x <- ages[widthvec]/365
-#print(x)
 y <- log(rowSums(pointsout)[widthvec])
-#print(log(rowSums(pointsout)))
-#print(y)
 z <- lm(y~x)
-#print(summary(z))
+
 
 
 tempsum <- sum(rowSums(pointsout),na.rm=TRUE)
 print("tempsum")
+print(pointsout)  
 print(rowSums(pointsout))
 print(tempsum) 
+
 #should follow what I did in Catch curve 1.
 par(1,las=1,bty='n',oma=c(0,1,1,1))
-#print("length check")
-#print(c(length(ages),length(log(rowSums(pointsout)))))
+#make the plots
 plot(ages/365,log(rowSums(pointsout)),xlab=bquote(paste("Relative age (t-t"[o],";year)")),ylab=expression(paste("Relative abundance (ln(N))")),yaxt="n",xaxt="n")
 points(ages[widthvec]/365,log(rowSums(pointsout)[widthvec]),pch=19,col="black") #make filled circles only on points in width vect
 axis(2,tck=0.02,las=2)
@@ -209,6 +249,8 @@ lines(x=ages[widthvec]/365,y=(z$coefficients[1]+z$coefficients[2]*ages[widthvec]
 
 temp2 <-bquote(paste("ln(N) = ",.(signif(z$coefficients[1],3)),.(signif(z$coefficients[2],3)),"*age"," ; ",r^2," = ",.(signif(summary(z)$r.squared,3)),";  sum",.(signif(tempsum,3))))  
 legend(x="topright",legend=temp2,inset=0.02)  
+
+
   
 x11() #useful plot for debugging 
 catchrqFreqPlot(1:days,datain$ML,lfdata,c(youngest,oldest,oldest+gcurve2$tzero,youngest+gcurve1$tzero),c(datain$ML[1],datain$ML[length(datain$ML)],0,0),tzero,gcurve1,gcurve2,gcurvemain,pointscurve,timeblue,datein,barscale=1,GF=0)
